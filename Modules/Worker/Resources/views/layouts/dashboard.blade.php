768663c7771950d403c395d462d0cda0d6463a44
Walid Lamniai <walid615@live.fr>
2024-02-06 18:12:57 +0300

    adding graph

--- Modules/Worker/Resources/views/layouts/dashboard.blade.php
+++ Modules/Worker/Resources/views/layouts/dashboard.blade.php
@@ -51,6 +51,37 @@
 
 <!--Main layout-->
     <!-- MDB -->
+
+    <main style="padding-top: 170px" class="ss-main-body-sec">
+    <div class="container">
+
+      <!--------Ghraph area------->
+        <div class="ss-home-graph-main-sec">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ss-home-graph-div1">
+                        <img src="{{URL::asset('frontend/img/home-graph-1.png')}}" />
+                    </div>
+                </div>
+
+                <div class="col-lg-7">
+                    <div class="ss-home-graph-div2">
+                        <img src="{{URL::asset('frontend/img/home-graph-2.png')}}" />
+                    </div>
+                </div>
+
+                <div class="col-lg-5">
+                    <div class="ss-home-graph-div3">
+                        <img src="{{URL::asset('frontend/img/home-graph-3.png')}}" />
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</main>
     <script type="text/javascript" src="{{URL::asset('frontend/js/mdb.min.js')}}"></script>
     <script type="text/javascript" src="{{ URL::asset('backend/vendors/confirm/jquery-confirm.min.js') }}" type="text/javascript"></script>
     {{-- <script type="text/javascript" src="{{ URL::asset('backend/vendors/datatables/jquery.dataTables.min.js') }}" type="text/javascript"></script> --}}
