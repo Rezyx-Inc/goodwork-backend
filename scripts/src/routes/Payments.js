4a5be544b38df2cb89c8f56f0cf8d33ee0ea3ec2
lodembeep <48884244+lodembeep@users.noreply.github.com>
2024-04-24 19:18:47 +0100

    Update Payments.js
    fix scope
    fix account types
    fix some indent
    partial support for customers and invoicing

--- scripts/src/routes/Payments.js
+++ scripts/src/routes/Payments.js
@@ -7,6 +7,8 @@ router.get('/', (req, res) => {
     res.send('Payments page');
 });
 
+/* Workers */
+
 // create a connected account | returns account id
 router.post('/create', async (req, res) => {
     
@@ -22,19 +24,21 @@ router.post('/create', async (req, res) => {
 	
 	    // Create the stripe connected account
 	    const account = await stripe.accounts.create({
-		  type: 'express',
+		  type: 'standard',
 		  country: 'US',
 		  email: req.body.email
 		});
 		
+		 // insert into users stripeId values account.id
+
+    	res.status(200).json({status: true, message:account.id});
+
 	}catch(e){
 		console.log(e);
 		return res.status(400).send({status: false, message: e.message})
 	}
 
-    // insert into users stripeId values account.id
-
-    res.status(200).json({status: true, message:account.id});
+   
 })
 
 // get - account links | consumes stripeId
@@ -47,19 +51,21 @@ router.get('/account-link', async (req, res) => {
     try{
 	
 	    const accountLink = await stripe.accountLinks.create({
-		  account: req.query.stripeId,
-		  refresh_url: process.env.REFRESH_URL_BASE_PATH + "/" + req.query.stripeId,
-		  return_url: process.env.RETURN_URL_BASE_PATH + "/" + req.query.stripeId,
-		  type: 'account_onboarding',
+			account: req.query.stripeId,
+			refresh_url: process.env.REFRESH_URL_BASE_PATH + "/" + req.query.stripeId,
+			return_url: process.env.RETURN_URL_BASE_PATH + "/" + req.query.stripeId,
+			type: 'account_onboarding'
 		});
+
+		// the expiracy is 5 minutes
+    	return res.status(200).json({status:true, message:accountLink.url})
 
 	}catch(e){
 		console.log(e);
 		return res.status(400).send({status: false, message: e.message})
 	}
     
-    // the expiracy is 5 minutes
-    return res.status(200).json({status:true, message:accountLink.url})
+    
 
 });
 
@@ -72,14 +78,16 @@ router.get('/login-link', async (req, res) => {
 
     try{
     
-    	const loginLink = await stripe.accounts.createLoginLink.create(req.query.stripeId);
+    	const loginLink = await stripe.accounts.createLoginLink(req.query.stripeId);
+    	
+    	return res.status(200).json({status:true, message:loginLink.url})
     
     }catch(e){
 		console.log(e);
 		return res.status(400).send({status: false, message: e.message})
 	}
     
-    return res.status(200).json({status:true, message:loginLink.url})
+    
 
 });
 
@@ -99,15 +107,95 @@ router.post('/transfer', async (req, res) => {
 	
 	    // Create the transfer
 	    const account = await stripe.transfers.create({
-		  amount:req.body.amount * 100,
+		  amount: Number(req.body.amount) * 100,
 		  currency: 'usd',
 		  destination: req.body.stripeId
 		});
 	
+		res.status(200).json({status: true, message:"OK"});
+
 	}catch(e){
 		console.log(e);
 		return res.status(400).send({status: false, message: e.message})
 	}
 
-    res.status(200).json({status: true, message:""});
+    
 })
+
+
+/* Recruiters - Employers (orgs) */
+
+// Create a customer
+router.post('/customer/create', async (req, res) => {
+
+	if(!Object.keys(req.body).length) {
+        return res.status(400).send({status:false, message:"Empty request"})
+    }
+    else if ( !req.body.email){
+
+        return res.status(400).send({status:false, message:"Missing parameter."})
+    }
+
+    let portal = "https://billing.stripe.com/p/login/test_8wMaFa19ddXy1wc5kk";
+
+    try{
+
+		const customer = await stripe.customers.create({
+			email: req.body.email,
+			payment_method: 'pm_card_visa',
+  			invoice_settings: {
+    			default_payment_method: 'pm_card_visa',
+  			}
+		});
+
+		// insert into users stripeId values customer.id
+		res.status(200).json({status: true, message:portal});
+
+	}catch(e){
+		console.log(e);
+		return res.status(400).send({status: false, message: e.message})
+	}
+
+});
+
+// Create an invoice
+router.post('/customer/create', async (req, res) => {
+
+	if(!Object.keys(req.body).length) {
+        return res.status(400).send({status:false, message:"Empty request"})
+    }
+    else if ( !req.body.stripeId || !req.body.amount){
+
+        return res.status(400).send({status:false, message:"Missing parameters."})
+    }
+
+    try{
+
+    	// Create invoice
+		const invoice = await stripe.invoices.create({
+			customer: req.body.stripeId,
+			auto_advance: false,
+			currency: "usd"
+		});
+
+		//create invoice item
+		const invoiceItem = await stripe.invoiceItems.create({
+			customer: req.body.stripeId,
+			amount: Number(req.body.amount) * 100,
+			invoice: invoice.id
+		});
+
+		// Finalize
+		await stripe.invoices.finalizeInvoice(invoice.id);
+
+		// insert into users stripeId values customer.id
+		res.status(200).json({status: true, message:portal});
+
+	}catch(e){
+		console.log(e);
+		return res.status(400).send({status: false, message: e.message})
+	}
+
+});
+
+module.exports = router;
