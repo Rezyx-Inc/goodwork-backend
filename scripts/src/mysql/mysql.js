c75cc7c0403d13e732f85cb9d96ab21b095ec804
lodembeep <48884244+lodembeep@users.noreply.github.com>
2024-05-09 21:07:21 +0100

    Update mysql.js
    fix auth

--- scripts/src/mysql/mysql.js
+++ scripts/src/mysql/mysql.js
@@ -2,5 +2,9 @@ require("dotenv").config();
 const mysql = require("mysql2")
 
 module.exports.pool = mysql.createPool(
-'mysql://u728345844_goodwork:PjZ:G2w&9hW[@localhost:3306/u728345844_goodwork'
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE
 ).promise();
