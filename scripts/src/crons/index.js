b80fc0cb0be8463d96745d4128c8ab4a28980a22
lodembeep <debugmed0@gmail.com>
2024-09-30 22:09:54 +0100

    Update index.js
    lint crons file

--- scripts/src/crons/index.js
+++ scripts/src/crons/index.js
@@ -1,37 +1,37 @@
-var cron = require('node-cron');
-var laboredge = require('./laboredge.js');
-var { report } = require('../set.js')
+var cron = require("node-cron");
+var laboredge = require("./laboredge.js");
+var { report } = require("../set.js");
 
 // Uncomment to seed accounts
 //laboredge.seed(999);
 
 //report("Hello from cron")
-laboredge.update();
+//laboredge.update();
 
-if(process.env.ENABLE_CRON){
-    console.log("Starting integrations cron jobs.")
+if (process.env.ENABLE_CRON) {
+  console.log("Starting integrations cron jobs.");
 
-    // Check newly added integrations every 10 minutes
-    cron.schedule('*/10 * * * *', () => {
-        console.log('Checking new integrations');
-        laboredge.init();
-    });
+  // Check newly added integrations every 10 minutes
+  cron.schedule("*/10 * * * *", () => {
+    console.log("Checking new integrations");
+    laboredge.init();
+  });
 
-    // Check updates every hour
-    cron.schedule('0 * * * *', () => {
-        console.log('Checking job updates');
-        laboredge.update();
-    });
+  // Check updates every hour
+  cron.schedule("0 * * * *", () => {
+    console.log("Checking job updates");
+    laboredge.update();
+  });
 
-    // Check other updates every day at 1 am
-    cron.schedule('0 1 * * *', () => {
-        console.log('Checking other updates');
-        laboredge.updateOthers();
-    });
+  // Check other updates every day at 1 am
+  cron.schedule("0 1 * * *", () => {
+    console.log("Checking other updates");
+    laboredge.updateOthers();
+  });
 
-    process.on('SIGTERM', function() {
-        report("Unexpected Crons exit")
-    });
-}else{
-    console.log("CRON jobs disabled.")
+  process.on("SIGTERM", function () {
+    report("Unexpected Crons exit");
+  });
+} else {
+  console.log("CRON jobs disabled.");
 }
