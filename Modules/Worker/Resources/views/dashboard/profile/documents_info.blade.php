b710f793ce34087bef0b378e0455ca8154f6c8cf
elabdellaoui-ismail <ismailelabdellaoui340@gmail.com>
2024-11-28 22:21:53 +0100

    worker profile => separate files

---
+++ Modules/Worker/Resources/views/dashboard/profile/documents_info.blade.php
@@ -0,0 +1,58 @@
+<div class="row justify-content-center">
+    {{-- Upload Document --}}
+    {{-- <div class="ss-form-group">
+                <label>Upload Document</label>
+                <input type="file" id="document_file" name="files" multiple
+                    required><br><br>
+                <label class="mt-2" for="file">Choose a file</label>
+            </div>
+            <span class="help-block-file"></span> --}}
+    <div class="ss-form-group "
+        style="
+                display: flex;
+                justify-content: right;
+                align-items: center;
+            ">
+        <span style="margin:0px; margin-right:20px;">Add your documents here
+            !</span>
+        <a href="#" onclick="open_modal(this)" class="ss-opr-mngr-plus-sec"
+            style="
+                    background: #3d2c39;
+                    width: 40px;
+                    height: 40px;
+                    line-height: 40px;
+                    text-align: center;
+                    border-radius: 100px;
+                    color: #52DEC1 !important;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    "
+            data-bs-toggle="modal" data-bs-target="#job-dtl-Dcouments"><i class="fas fa-plus"></i></a>
+
+
+        <br><br>
+
+    </div>
+    {{-- manage file table --}}
+    <table style="font-size: 16px;" class="table row">
+        <thead>
+            <tr class="row">
+                <th class="col-3">Document Name</th>
+                <th class="col-3">Type</th>
+                <th class="col-3">View</th>
+                <th class="col-3">Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+
+    {{-- Skip && Save --}}
+    <div class="ss-prsn-form-btn-sec">
+        <button type="text" class="ss-prsnl-skip-btn prev-2"> Previous
+        </button>
+        <button id="uploadForm" class="ss-prsnl-save-btn next-2"> Save Files
+        </button>
+    </div>
+</div>
---
+++ Modules/Worker/Resources/views/dashboard/profile/personal_info.blade.php
@@ -0,0 +1,70 @@
+<div class="row justify-content-center">
+    {{-- First Name --}}
+    <div class="ss-form-group col-11">
+        <label>First Name</label>
+        <input type="text" name="first_name" placeholder="Please enter your first name"
+            value="{{ isset($user->first_name) ? $user->first_name : '' }}">
+    </div>
+    <span class="help-block-first_name"></span>
+    {{-- Last Name --}}
+    <div class="ss-form-group col-11">
+        <label>Last Name</label>
+        <input type="text" name="last_name" placeholder="Please enter your last name"
+            value="{{ isset($user->last_name) ? $user->last_name : '' }}">
+    </div>
+    <span class="help-block-last_name"></span>
+    {{-- Phone Number --}}
+    <div class="ss-form-group col-11">
+        <label>Phone Number</label>
+        <input id="contact_number" type="text" name="mobile" placeholder="Please enter your phone number"
+            value="{{ isset($user->mobile) ? $user->mobile : '' }}">
+    </div>
+    <span class="help-block-mobile"></span>
+    {{-- Address Information --}}
+    <div class="ss-form-group col-11">
+        <label>Address</label>
+        <input type="text" name="address" placeholder="Please enter your address"
+            value="{{ isset($worker->address) ? $worker->address : '' }}">
+    </div>
+    <span class="help-block-address"></span>
+    {{-- State Information --}}
+    <div class="ss-form-group col-11">
+        <label>State</label>
+        <select name="state" id="job_state">
+            <option value="{{ !empty($worker->state) ? $worker->state : '' }}" disabled selected hidden>
+                {{ !empty($worker->state) ? $worker->state : 'What State are you located in?' }}
+            </option>
+            @foreach ($states as $state)
+                <option id="{{ $state->id }}" value="{{ $state->name }}">
+                    {{ $state->name }}
+                </option>
+            @endforeach
+        </select>
+    </div>
+    <span class="help-block-state"></span>
+    {{-- City Information --}}
+    <div class="ss-form-group col-11">
+        <label>City</label>
+        <select name="city" id="job_city">
+            <option value="{{ !empty($worker->city) ? $worker->city : '' }}" disabled selected hidden>
+                {{ !empty($worker->city) ? $worker->city : 'What City are you located in?' }}
+            </option>
+        </select>
+    </div>
+    <span class="help-block-city"></span>
+    <span class="help-city">Please select a state first</span>
+    {{-- Zip Code Information --}}
+    <div class="ss-form-group col-11">
+        <label>Zip Code</label>
+        <input type="number" name="zip_code" placeholder="Please enter your Zip Code"
+            value="{{ isset($user->zip_code) ? $user->zip_code : '' }}">
+    </div>
+    <span class="help-block-zip_code"></span>
+    {{-- Skip && Save --}}
+    <div class="ss-prsn-form-btn-sec col-11">
+        <button type="text" class="ss-prsnl-save-btn" id="SaveBaiscInformation"> Save
+        </button>
+        <button type="text" class="ss-prsnl-save-btn firstNext"> Next
+        </button>
+    </div>
+</div>
