167706dfa7f116b98e15d3605a91b57d6660e799
yassine <yassineova@gmail.com>
2024-12-17 17:15:20 +0100

    add cleared to start for recruiter
    - fix route bug
    - add cleared to start in dashboard

--- Modules/Recruiter/Resources/views/recruiter/applicationjourney.blade.php
+++ Modules/Recruiter/Resources/views/recruiter/applicationjourney.blade.php
@@ -40,6 +40,13 @@
                         <span>{{ $statusCounts['Onboarding'] }} {{ $statusCounts['Onboarding'] == 1 ? 'Application' : 'Applications' }}</span>
                     </div>
                 </div>
+                {{-- Cleared Applicants --}}
+                <div style="flex: 1 1 0px;">
+                    <div class="ss-job-prfle-sec" onclick="selectOfferCycleState('Cleared')" id="Cleared">
+                        <p>Cleared</p>
+                        <span>{{ $statusCounts['Cleared'] }} {{ $statusCounts['Cleared'] == 1 ? 'Application' : 'Applications' }}</span>
+                    </div>
+                </div>
                 {{-- Working Applicants --}}
                 <div style="flex: 1 1 0px;">
                     <div class="ss-job-prfle-sec" onclick="selectOfferCycleState('Working')" id="Working">
@@ -339,6 +346,7 @@
             var submittedElement = document.getElementById('Submitted');
             var offeredElement = document.getElementById('Offered');
             var onboardingElement = document.getElementById('Onboarding');
+            var clearedElement = document.getElementById('Cleared');
             var workingElement = document.getElementById('Working');
             var doneElement = document.getElementById('Done');
             var holdElement = document.getElementById('Hold');
@@ -360,6 +368,9 @@
             }
             if (onboardingElement.classList.contains("active")) {
                 onboardingElement.classList.remove("active");
+            }
+            if (clearedElement.classList.contains("active")) {
+                clearedElement.classList.remove("active");
             }
             if (workingElement.classList.contains("active")) {
                 workingElement.classList.remove("active");
@@ -1190,6 +1201,8 @@
         }
 
         function selectOfferCycleState(type){
+            console.log("type=================", type);
+            
             applicationStatusToggle(type);
             var csrfToken = $('meta[name="csrf-token"]').attr('content');
             if (csrfToken) {
@@ -1273,6 +1286,7 @@
             var submittedElement = document.getElementById('Submitted');
             var offeredElement = document.getElementById('Offered');
             var onboardingElement = document.getElementById('Onboarding');
+            var clearedElement = document.getElementById('Cleared');
             var workingElement = document.getElementById('Working');
             var doneElement = document.getElementById('Done');
             var holdElement = document.getElementById('Hold');
@@ -1293,6 +1307,9 @@
             }
             if (onboardingElement.classList.contains("active")) {
                 onboardingElement.classList.remove("active");
+            }
+            if (clearedElement.classList.contains("active")) {
+                clearedElement.classList.remove("active");
             }
             if (workingElement.classList.contains("active")) {
                 workingElement.classList.remove("active");
@@ -1321,7 +1338,12 @@
             if(type == 'Apply'){
                 document.getElementById('listingname').innerHTML = 'New Applications';
             }else if (type != null){
-                document.getElementById('listingname').innerHTML = type + ' Applications';
+                if (type == 'Cleared') {
+                    document.getElementById('listingname').innerHTML = 'Cleared to Start Applications';
+
+                }else{
+                    document.getElementById('listingname').innerHTML = type + ' Applications';
+                }
             }
             if (type == 'Done' || type == 'Rejected' || type == 'Blocked' || type == 'Hold') {
 
@@ -1446,7 +1468,7 @@
                             text: result.message,
                             time: 5
                         });
-                        const statusKeys = ['Apply', 'Screening', 'Submitted', 'Offered', 'Onboarding', 'Working', 'Rejected', 'Blocked', 'Hold'];
+                        const statusKeys = ['Apply', 'Screening', 'Submitted', 'Offered', 'Onboarding', 'Cleared', 'Working', 'Rejected', 'Blocked', 'Hold'];
 
                         statusKeys.forEach(key => {
                             $(`#${key} span`).text(`${result.statusCounts[key]} Applicants`);
@@ -1543,7 +1565,7 @@
                             text: result.message,
                             time: 5
                         });
-                        const statusKeys = ['Apply', 'Screening', 'Submitted', 'Offered', 'Onboarding', 'Working', 'Rejected', 'Blocked', 'Hold'];
+                        const statusKeys = ['Apply', 'Screening', 'Submitted', 'Offered', 'Onboarding', 'Cleared', 'Working', 'Rejected', 'Blocked', 'Hold'];
 
                         statusKeys.forEach(key => {
                             $(`#${key} span`).text(`${result.statusCounts[key]} Applicants`);
