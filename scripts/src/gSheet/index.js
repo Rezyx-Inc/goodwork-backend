06a68c78c08ec4a29f8545185bd3955491357c40
yassine <yassineova@gmail.com>
2024-12-20 00:55:49 +0100

    add daniel stull jobs to staging db

--- scripts/src/gSheet/index.js
+++ scripts/src/gSheet/index.js
@@ -6,178 +6,195 @@ var _ = require('lodash');
 const { json } = require('body-parser');
 const { exit } = require('process');
 const { deleteAllSpreadsheets, deleteSpreadsheetById, addDataToSpreadsheet } = require('./crud/crud.js');
-
+const csv = require('csvtojson');
 const queries = require("../mysql/sheet.js");
 
+const axios = require('axios');
 const { authorize } = require('./services/authService');
 
 async function getDataAndSaveAsJson(auth, spreadsheetId, spreadsheetName) {
   try {
 
 
-    const sheets = google.sheets({ version: 'v4', headers: { Authorization: `Bearer ${auth}` } });
-    const res = await sheets.spreadsheets.values.get({
-      spreadsheetId: spreadsheetId,
-      range: `Sheet1!A:BH`,
-      // auth: auth,
-    });
+    // const sheets = google.sheets({ version: 'v4', headers: { Authorization: `Bearer ${auth}` } });
+    // const res = await sheets.spreadsheets.values.get({
+    //   spreadsheetId: spreadsheetId,
+    //   range: `Sheet1!A:BH`,
+    //   // auth: auth,
+    // });
 
-    // Access the rows from the values property
-    var rows = res.data.values;
+    // // Access the rows from the values property
+    // var rows = res.data.values;
 
-    //console.log(rows.slice(1).length);
+    // //console.log(rows.slice(1).length);
 
-    if (!rows || rows.length === 0) {
-      console.log('No data found.');
-      return;
-    }
-    const headers = rows.shift();
+    // if (!rows || rows.length === 0) {
+    //   console.log('No data found.');
+    //   return;
+    // }
+    // const headers = rows.shift();
 
-    // Transform the data into an array of objects
-    const jsonData = rows.map(row => {
-      const rowObject = {};
+    // // Transform the data into an array of objects
+    // const jsonData = rows.map(row => {
+    //   const rowObject = {};
 
-      headers.forEach((header, index) => {
-        let value = row[index] || '';
+    //   headers.forEach((header, index) => {
+    //     let value = row[index] || '';
 
-        // Check if the value is fully numeric
-        const isNumeric = /^\d+$/.test(value);
+    //     // Check if the value is fully numeric
+    //     const isNumeric = /^\d+$/.test(value);
 
-        // Check if the value is boolean (case insensitive)
-        const isBoolean = value.toLowerCase() === 'true' || value.toLowerCase() === 'false';
+    //     // Check if the value is boolean (case insensitive)
+    //     const isBoolean = value.toLowerCase() === 'true' || value.toLowerCase() === 'false';
 
-        // Check if the value is in "M/D/YYYY", "MM/DD/YYYY", "M-D-YYYY", or "MM-DD-YYYY" format
-        const isDateFormat = /^\d{1,2}[\/-]\d{1,2}[\/-]\d{4}$/.test(value);
+    //     // Check if the value is in "M/D/YYYY", "MM/DD/YYYY", "M-D-YYYY", or "MM-DD-YYYY" format
+    //     const isDateFormat = /^\d{1,2}[\/-]\d{1,2}[\/-]\d{4}$/.test(value);
 
-        // Assign based on type
-        if (header === 'Job ID') {
-          rowObject[header] = value; // Keep 'job_id' as a string
-        } else if (isNumeric) {
-          rowObject[header] = parseFloat(value); // Convert to number if fully numeric
-        } else if (isBoolean) {
-          rowObject[header] = value.toLowerCase() === 'true'; // Convert to boolean
-        } else if (isDateFormat) {
-          // Convert "M/D/YYYY", "MM/DD/YYYY", "M-D-YYYY", or "MM-DD-YYYY" to "YYYY-MM-DD"
-          const [month, day, year] = value.split(/[\/-]/);
-          const formattedMonth = month.padStart(2, '0');
-          const formattedDay = day.padStart(2, '0');
-          rowObject[header] = `${year}-${formattedMonth}-${formattedDay}`;
-        } else if (value === '') {
-          rowObject[header] = null; // Set empty values to null
-        } else {
-          rowObject[header] = value; // Keep as string for all other cases
-        }
-      });
+    //     // Assign based on type
+    //     if (header === 'Job ID') {
+    //       rowObject[header] = value; // Keep 'job_id' as a string
+    //     } else if (isNumeric) {
+    //       rowObject[header] = parseFloat(value); // Convert to number if fully numeric
+    //     } else if (isBoolean) {
+    //       rowObject[header] = value.toLowerCase() === 'true'; // Convert to boolean
+    //     } else if (isDateFormat) {
+    //       // Convert "M/D/YYYY", "MM/DD/YYYY", "M-D-YYYY", or "MM-DD-YYYY" to "YYYY-MM-DD"
+    //       const [month, day, year] = value.split(/[\/-]/);
+    //       const formattedMonth = month.padStart(2, '0');
+    //       const formattedDay = day.padStart(2, '0');
+    //       rowObject[header] = `${year}-${formattedMonth}-${formattedDay}`;
+    //     } else if (value === '') {
+    //       rowObject[header] = null; // Set empty values to null
+    //     } else {
+    //       rowObject[header] = value; // Keep as string for all other cases
+    //     }
+    //   });
 
-      return rowObject;
-    });
+    //   return rowObject;
+    // });
 
     // Prepare filename
-    const fileName = `${spreadsheetName.replace(/[<>:"/\\|?*]/g, '_')}.json`;
-    const folderPath = path.join(__dirname, 'jsons');
-    const filePath = path.join(folderPath, fileName);
+    // const fileName = `${spreadsheetName.replace(/[<>:"/\\|?*]/g, '_')}.json`;
+    // const folderPath = path.join(__dirname, 'jsons');
+    // const filePath = path.join(folderPath, fileName);
+    var json = await csv().fromFile(path.join(__dirname, "Test Jobs.csv"));
 
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath);
-    }
-    let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
-    console.log(OrgaId);
+    //console.log(json);
+    //return ;
+    // if (!fs.existsSync(folderPath)) {
+    //   fs.mkdirSync(folderPath);
+    // }
+    // //let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
+    // //console.log(OrgaId);
 
-    // Check if the JSON file already exists
-    const existFile = fs.existsSync(filePath);
+    // // Check if the JSON file already exists
+    // const existFile = fs.existsSync(filePath);
 
-    if (existFile) {
-      // Read the existing file
-      const oldFile = await fs.promises.readFile(filePath, 'utf8');
-      const old_File_Parsed = JSON.parse(oldFile);
+    // if (existFile) {
+    //   // Read the existing file
+    //   const oldFile = await fs.promises.readFile(filePath, 'utf8');
+    //   const old_File_Parsed = JSON.parse(oldFile);
 
-      if (_.isEqual(old_File_Parsed, jsonData)) {
-        console.log('No changes');
+    //   if (_.isEqual(old_File_Parsed, jsonData)) {
+    //     console.log('No changes');
 
-      } else {
-        // Delete rows
-        for (let i in old_File_Parsed) {
-          let oldRow = old_File_Parsed[i];
-          let id = oldRow["Org Job Id"];
-          let newRow = jsonData.find((newRow) => newRow["Org Job Id"] === id);
+    //   } else {
+    //     // Delete rows
+    //     for (let i in old_File_Parsed) {
+    //       let oldRow = old_File_Parsed[i];
+    //       let id = oldRow["Org Job Id"];
+    //       let newRow = jsonData.find((newRow) => newRow["Org Job Id"] === id);
 
-          if (!newRow) {
-            console.log('Row deleted:', oldRow["Org Job Id"]);
+    //       if (!newRow) {
+    //         console.log('Row deleted:', oldRow["Org Job Id"]);
 
-            let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
-            try {
-              await queries.deleteJob(OrgaId, oldRow["Org Job Id"],);
+    //         let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
+    //         try {
+    //           await queries.deleteJob(OrgaId, oldRow["Org Job Id"],);
 
-            } catch (err) {
-              console.error(`Error in job with ID ${oldRow["Org Job Id"]}:`, err);
-            }
+    //         } catch (err) {
+    //           console.error(`Error in job with ID ${oldRow["Org Job Id"]}:`, err);
+    //         }
 
-          }
-        }
+    //       }
+    //     }
 
-        // Update & add rows
-        for (let i in jsonData) {
-          let newRow = jsonData[i];
-          let id = newRow["Org Job Id"];
-          let oldRow = old_File_Parsed.find((j) => j["Org Job Id"] === id);
-
-
-          if (oldRow) {
-            if (_.isEqual(newRow, oldRow)) {
-              continue;
-            } else {
-              console.log('Row updated:', newRow["Org Job Id"]);
-              // update data in the database
-              // get the organization ID
-              let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
-
-              await queries.updateJob(OrgaId, newRow);
-
-            }
-          } else {
-            console.log('Row added:', newRow["Org Job Id"]);
-
-            // insert data in the database
-            // get the organization ID
-            let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
-            try {
-              await queries.insertJob(OrgaId, newRow);
-
-            } catch (err) {
-              console.error(`Error add job with ID ${OrgaId}:`, err);
-            }
-
-          }
-        }
+    //     // Update & add rows
+    //     for (let i in jsonData) {
+    //       let newRow = jsonData[i];
+    //       let id = newRow["Org Job Id"];
+    //       let oldRow = old_File_Parsed.find((j) => j["Org Job Id"] === id);
 
 
+    //       if (oldRow) {
+    //         if (_.isEqual(newRow, oldRow)) {
+    //           continue;
+    //         } else {
+    //           console.log('Row updated:', newRow["Org Job Id"]);
+    //           // update data in the database
+    //           // get the organization ID
+    //           let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
 
-        // Save the updated data
-        await fs.promises.writeFile(filePath, JSON.stringify(jsonData, null, 2));
-        console.log(`Updated data saved to ${filePath}`);
-      }
-    } else {
+    //           await queries.updateJob(OrgaId, newRow);
 
-      // Save the JSON data to a new file in the jsons folder
-      await fs.promises.writeFile(filePath, JSON.stringify(jsonData, null, 2));
-      console.log(`Data saved to ${filePath}`);
+    //         }
+    //       } else {
+    //         console.log('Row added:', newRow["Org Job Id"]);
+
+    //         // insert data in the database
+    //         // get the organization ID
+    //         let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
+    //         try {
+    //           await queries.insertJob(OrgaId, newRow);
+
+    //         } catch (err) {
+    //           console.error(`Error add job with ID ${OrgaId}:`, err);
+    //         }
+
+    //       }
+    //     }
 
 
-      // insert data in the database
 
-      // get the organization ID
-      let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
+    //     // Save the updated data
+    //     await fs.promises.writeFile(filePath, JSON.stringify(jsonData, null, 2));
+    //     console.log(`Updated data saved to ${filePath}`);
+    //   }
+    // } else {
 
-      for (const job of jsonData) {
+    // // Save the JSON data to a new file in the jsons folder
+    //await fs.promises.writeFile(filePath, JSON.stringify(jsonData, null, 2));
+    // console.log(`Data saved to ${filePath}`);
+
+
+    // insert data in the database
+
+    // get the organization ID
+    //let OrgaId = spreadsheetName.match(/\[(.*?)\]/)[1];
+    const OrgaId = "GWU000007";
+
+    //let jobdbId = await queries.insertJob(OrgaId, job);
+    let recruiterID = null;
+
+    recruiterID = await axios.post("http://localhost:4545/organizations/assignUpNextRecruiter", { id: OrgaId });
+
+    if (recruiterID !== "Up next recruiter not found.") {
+      for (const job of json) {
         try {
+
           await queries.insertJob(OrgaId, job);
 
+          await queries.updateJobRecruiterID(job["Org Job Id"], recruiterID.data);
 
         } catch (err) {
           console.error(`Error in job with ID ${job["Org Job Id"]}:`, err);
         }
       }
+    }else {
+      console.log("No recruiter found for this organization");
     }
+    
+    //}
 
 
 
@@ -224,7 +241,7 @@ async function processAllSpreadsheets(auth) {
 
       if (spreadsheet.id === "1YsIGVl2l19r_j-bFkm7aWSnUNZXPf19HWcoBHO_xqc4") {
         console.log("skip this");
-        
+
       } else {
         console.log(`Processing spreadsheet: ${spreadsheet.name} (${spreadsheet.id})`);
 
@@ -245,7 +262,9 @@ async function main() {
   try {
     const auth = await authorize();
 
-    await processAllSpreadsheets(auth.credentials.access_token);
+    //await processAllSpreadsheets(auth.credentials.access_token);
+
+    await getDataAndSaveAsJson()
 
     const id_for_add = "1IWv1voLSTzIRWZkBj4wB0PyYAQdU0-nOQ4Y28wY9xC4"
     //await addDataToSpreadsheet(auth.credentials.access_token, id_for_add);
