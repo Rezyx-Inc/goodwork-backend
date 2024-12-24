fee9245bdf0569d5f7609524693d29a7036f5562
lodembeep <debugmed0@gmail.com>
2024-12-24 20:45:11 +0100

    Update app.js
    Use the new reporter

--- scripts/app.js
+++ scripts/app.js
@@ -5,16 +5,17 @@ require("dotenv").config();
 
 var { report } = require('./src/set.js')
 
+
 const server = path.join(path.dirname(__filename), "server.js")
 const crons = path.join(path.dirname(__filename), "/src/crons/index.js")
 const webhooks = path.join(path.dirname(__filename), "/src/webhooks/stripe.js")
 
 const { argv } = require('node:process');
 
-pm2.connect(function(err) {
+pm2.connect( async function(err) {
   if (err) {
     console.error(err)
-    report("Unable to connect PM2")
+    await report('error','app.js',"Unable to connect PM2")
     process.exit(2)
   }
 
@@ -29,84 +30,90 @@ pm2.connect(function(err) {
     console.log("Starting the File Management service.")
 
     // Files management
-    pm2.start({
+    await pm2.start({
       script    : server,
       name      : 'files',
       watch     : watch
-    }, function(err, apps) {
+    }, async function(err, apps) {
       if (err) {
         console.error(err)
-        report("Unable to start File management")
+        await report('error','app.js',"Unable to start File management")
         return pm2.disconnect()
 
       }
+      await report("notification", 'app.js', 'Started Files management service');
       return pm2.disconnect()
     });
 
     console.log("Starting the Webhooks service")
     // Webhook management
-    pm2.start({
+    await pm2.start({
       script    : webhooks,
       name      : 'webhooks',
       watch     : watch
-    }, function(err, apps) {
+    }, async function(err, apps) {
       if (err) {
         console.error(err)
-        report("Unable to start Webhook service")
+        await report('error','app.js',"Unable to start Webhook service")
         return pm2.disconnect()
 
       }
+
+      await report("notification", 'app.js', 'Started Webhooks service');
       return pm2.disconnect()
     });
 
     // Crons management
     if(process.env.ENABLE_CRONS == true){
       console.log("Starting the Cron service.")
-      pm2.start({
+      await pm2.start({
         script    : crons,
         name      : 'crons',
         watch     : watch
-      }, function(err, apps) {
+      }, async function(err, apps) {
         if (err) {
           console.error(err)
-          report("Unable to start Cron service")
+          await report('error','app.js',"Unable to start Cron service")
           return pm2.disconnect()
         }
+        await report("notification", 'app.js', 'Started Crons service');
         return pm2.disconnect()
       });
     }
-  
+
   }else if(argv[2] === "stop"){
 
     console.log("Stopping the file management and integration services")
 
     // List the processes managed by PM2 then delete
-    pm2.list((err, list) => {
+    await pm2.list(async (err, list) => {
       if(list.length == 0){
         console.log("No process to stop.")
         pm2.disconnect()
-      }    
+      }
       if(!err){
 
         for(proc of list){
-          pm2.delete(proc.name, (err, proc) => {
+          pm2.delete(proc.name, async (err, proc) => {
+
+            await report("notification", 'app.js', 'Stopped a service');
             // Disconnects from PM2
             pm2.disconnect()
-          });  
+          });
         }
 
       }else{
         console.log("Unable to list processes", err)
-        report("Unable to list PM2 processes")
+        await report('error','app.js',"Unable to list PM2 processes")
         pm2.disconnect()
       }
     })
 
   }else{
     console.error("Invalid argument")
-    report("Invalid app argument")
+    await report('error','app.js',"Invalid app argument")
   }
-  
+
 })
 
 
