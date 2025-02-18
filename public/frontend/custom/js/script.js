effd9f3e40fc5e65d4cea53cfbe011b849ad979f
ELABDELLAOUI-ismail <86296414+ELABDELLAOUI-ismail@users.noreply.github.com>
2025-02-18 16:15:18 +0100

    remove notif

--- public/frontend/custom/js/script.js
+++ public/frontend/custom/js/script.js
@@ -653,13 +653,6 @@ function update_nurse_information(data_to_send, notify = false)
         data: data_to_send,
         success: function (resp) {
             ajaxindicatorstop();
-            // if (notify && resp.success) {
-            //     notie.alert({
-            //         type: 'success',
-            //         text: '<i class="fa fa-check"></i> Data Saved.',
-            //         time: 2
-            //     });
-            // }
         },
         error: function (resp) {
             ajaxindicatorstop();
