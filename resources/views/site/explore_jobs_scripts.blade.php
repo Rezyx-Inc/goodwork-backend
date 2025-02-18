6c0a2cd26732801824ff8b8e2ea3ce554a646dc6
ELABDELLAOUI-ismail <86296414+ELABDELLAOUI-ismail@users.noreply.github.com>
2025-02-18 13:09:23 +0100

    fix all ysers

--- resources/views/site/explore_jobs_scripts.blade.php
+++ resources/views/site/explore_jobs_scripts.blade.php
@@ -941,9 +941,9 @@
     function addJobCards(jobs){
 
         for(job of jobs.jobs){
-            var escapedJob = JSON.stringify(job).replaceAll("\"", "'");
-
-            var newCard = '<div class="ss-job-prfle-sec job-item" data-users="'+ JSON.stringify(allUsers).replaceAll("\"", "'") +'" data-id="'+ job.id +'" data-job="'+ escapedJob +'">'+
+            
+            var newCard =
+            '<div class="ss-job-prfle-sec job-item" data-id="'+job.id+'" >'+
                 '<div class="row d-flex align-items-center">'+
                     '<p class="col-12 text-end d-sm-none" style="padding-right:20px;">'+
                         '<span>+'+ job.offer_count +' Applied</span>'+
@@ -952,8 +952,7 @@
                         '<div class="infos_like_ul">';
 
                         if(job.profession){
-                            newCard += '<li><a href="#"><svg style="vertical-align: sub;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16">'+
-                                '<path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5"/></svg> '+ job.profession +'</a></li>';
+                            newCard += '<li><a href="#"><svg style="vertical-align: sub;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16"> <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5m1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0M1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5" /> </svg> '+ job.profession +'</a></li>';
                         }
                         if(job.preferred_specialty){
                             newCard += '<li><a href="#"> '+ job.preferred_specialty +'</a></li>';
