6e890b4d98127e18d6ec54ef379e1a5bcab3fe7d
MOHAMMED AMINE ELHARCHI <107551708+MohammedAmine-ELHARCHI@users.noreply.github.com>
2025-02-20 16:39:06 +0100

    remove dns

--- Modules/Worker/Http/Controllers/WorkerAuthController.php
+++ Modules/Worker/Http/Controllers/WorkerAuthController.php
@@ -149,7 +149,7 @@ class WorkerAuthController extends Controller
                     'last_name' => 'required|regex:/^[a-zA-Z\s]+$/|max:255',
                     'mobile' => ['nullable', 'regex:/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/'],
                     //needs net` access
-                    'email' => 'email:rfc,dns',
+                    'email' => 'email:rfc',
                     'profession' => 'required',
                 ]);
                 if ($validator->fails()) {
