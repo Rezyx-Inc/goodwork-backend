1a7bf668a32292d48ddd1825488285b4aaaf4eef
MOHAMMED AMINE ELHARCHI <107551708+MohammedAmine-ELHARCHI@users.noreply.github.com>
2025-02-20 15:52:59 +0100

    Add profession to worker signup & Fix combining auth bugs

--- routes/web.php
+++ routes/web.php
@@ -44,11 +44,12 @@ Route::middleware(['web'])->group(function () {
 
     Route::get('clear-cache', 'SiteController@clear_cache');
 
+    Route::get('signup', ['uses' => 'SiteController@signup', 'as' => 'signup']);
+    Route::get('login', ['uses' => 'SiteController@login', 'as' => 'login']);
+    
     Route::middleware(['user_not_logged_in'])->group(function () {
         /* Registration and authentication routes */
-        Route::get('signup', ['uses' => 'SiteController@signup', 'as' => 'signup']);
         Route::post('signup', ['uses' => 'SiteController@post_signup', 'as' => 'signup.store']);
-        Route::get('login', ['uses' => 'SiteController@login', 'as' => 'login']);
         Route::post('login', ['uses' => 'SiteController@post_login', 'as' => 'login.store']);
         Route::get('otp', ['uses' => 'SiteController@otp', 'as' => 'otp']);
         Route::post('otp', ['uses' => 'SiteController@submit_otp', 'as' => 'otp.store']);
